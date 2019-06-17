b522bed61904d620733e293ef3e7867d3624623e
Haydn Ewers <27211+haydn@users.noreply.github.com>
2019-06-17 11:21:01 +1000

    Fix type declarations

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,19 @@
 declare module "use-state-snapshots" {
-  interface Snapshot<T> {
-    id: number;
-    state: T;
-    firstChange: number;
-    lastChange: number;
-  }
-
-  type useStateSnapshots = <T>(
+  function useStateSnapshots<T>(
     initial: T | (() => T),
     delay?: number | false,
     limit?: number
-  ) => [
+  ): [
     T,
     (action: T | ((state: T) => T), force?: boolean) => void,
     number,
     (action: number | ((pointer: number) => number)) => void,
-    Array<Snapshot<T>>
+    Array<{
+      id: number;
+      state: T;
+      firstChange: number;
+      lastChange: number;
+    }>
   ];
-
   export = useStateSnapshots;
 }
